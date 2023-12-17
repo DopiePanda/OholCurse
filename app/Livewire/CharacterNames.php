aa85f403346588347693847014594ce12191882e
oholcurse <oholcurse@shell>
2023-12-17 04:34:45 +0100

    Added game objects to admin panel

--- app/Livewire/CharacterNames.php
+++ app/Livewire/CharacterNames.php
@@ -4,8 +4,13 @@ namespace App\Livewire;
 
 use Livewire\Component;
 
+use Carbon\Carbon;
+use DB;
+
 use App\Models\FirstName;
 use App\Models\LastName;
+use App\Models\LifeLog;
+use App\Models\LifeNameLog;
 
 class CharacterNames extends Component
 {
@@ -16,11 +21,16 @@ class CharacterNames extends Component
     public $first_names;
     public $last_names;
 
+    public $top_first_names;
+    public $top_last_names;
+
     public function mount()
     {
         $this->gender = 'female';
         $this->randomFirstName();
         $this->randomLastName();
+
+        //$this->mostPopularFirstNames();
     }
 
     public function render()
@@ -120,5 +130,55 @@ class CharacterNames extends Component
         }
         
         
+    }
+
+    public function mostPopularFirstNames()
+    {
+        $start = Carbon::now();
+        $end = $start->clone()->addMonths(1);
+
+        /* $this->top_first_names = LifeLog::query()   
+                                    ->with(['name' => function ($query) {
+                                        $query->select('character_id', 'name')
+                                        ->selectRaw('COUNT(*) AS count')
+                                        ->where('name', 'NOT LIKE', 'EVE%')
+                                        
+                                        ->groupBy('name')
+                                        ->orderBy('count', 'desc');
+
+                                        }])
+                                    ->whereHas('name', function ($query) {
+                                        $query->where('name', '!=', null);
+                                    })
+                                    ->where('timestamp', '>=', Carbon::now()->subMonth()->timestamp)
+                                    ->where('type', '=', 'birth')
+                                    ->limit(10)
+                                    ->get(); */
+
+        $this->top_first_names = DB::table('life_name_logs')
+                                    ->join('life_logs', 'life_name_logs.character_id', '=', 'life_logs.character_id')
+                                    ->select(DB::raw("(COUNT(life_name_logs.name)) as count"), 'life_name_logs.character_id', 'life_name_logs.name', 'life_logs.timestamp', 'life_logs.type', 'life_logs.gender')
+                                    ->where('life_logs.type', 'birth')
+                                    ->where('life_logs.timestamp', '>=', $start->subMonth()->timestamp)
+                                    ->where('life_name_logs.name', 'not like', 'EVE%')
+                                    ->where('life_name_logs.name', '!=', null)
+                                    ->groupBy('life_name_logs.name')
+                                    ->orderBy('count', 'desc')
+                                    ->limit(10)
+                                    ->get();
+
+        $names = LifeLog::whereHas('name')
+                    ->with(['name' => function ($query) {
+                        $query->select('character_id', 'name')
+                        ->where('name', 'NOT LIKE', 'EVE%');
+                    }])
+                    ->select('character_id')
+                    ->where('timestamp', '>=', Carbon::now()->subMonth()->timestamp)
+                    ->where('type', '=', 'birth')
+                    ->limit(100)
+                    ->get();
+
+
+        dd($names);
     }
 }
