0c2dc7a608039f51c3e22562fc253a9ce8616437
oholcurse <oholcurse@shell.domeneshop.no>
2024-04-24 16:15:14 +0200

    Added pill to display article category

--- resources/views/livewire/news/index.blade.php
+++ resources/views/livewire/news/index.blade.php
@@ -11,8 +11,11 @@
             @foreach($articles as $article)
                 <div class="max-w-96 p-2 border border-gray-400 flex flex-col">
                     <a class="block w-full h-full" href="{{ route('news.article', ['id' => $article->id, 'slug' => $article->slug]) }}">
-                        <div class="mt-2 h-60 border-2 border-black grow">
+                        <div class="relative mt-2 h-60 border-2 border-black grow">
                             <img class="object-cover w-full h-full" src="{{ asset($article->images->first()->image_url ?? 'MISSING') }}" />
+                            <div class="absolute left-0 bottom-0 ml-1 mb-1 px-2 text-sm rounded-full bg-red-500 uppercase text-white">
+                                {{ $article->type }}
+                            </div>
                         </div>
                         <div class="py-2 font-serif text-xl font-bold">
                             {{ $article->title }}
@@ -22,87 +25,29 @@
 
                 @foreach($ads as $ad)
                     @if($loop->parent->index == $ad->index)
-                        <div class="grow max-w-96 p-2 border border-gray-400 flex flex-col">
-                            <div class="mt-2 h-60 border-8 border-black grow">
+                        <div class="max-w-96 p-2 border border-gray-400 flex flex-col">
+                            @if($ad->url)
+                                <a href="{{ $ad->url }}">
+                            @endif
+                                
+                            <div class="relative mt-2 h-60 border-8 border-black grow">
                                 <img class="object-cover w-full h-full" src="{{ asset($ad->image_url) }}" />
+                                <div class="absolute left-0 bottom-0 ml-1 mb-1 px-2 text-sm rounded-full bg-red-500 uppercase text-white">
+                                    AD
+                                </div>
                             </div>
                             <div class="py-2 uppercase text-center font-serif text-xl font-bold">
                                 {{ $ad->title }}
                             </div>
+
+                            @if($ad->url)
+                                </a>
+                            @endif
                         </div>
                     @endif
                 @endforeach
 
             @endforeach
-        </div>
-
-        <!-- AD TEMPLATE
-
-        <div class="grow max-w-96 p-2 border border-gray-400 flex flex-col">
-            <div class="mt-2 h-60 border-8 border-black grow">
-                <img class="object-cover w-full h-full" src="{{ asset('/assets/news-articles/ad-1.jpg') }}" />
-            </div>
-            <div class="py-2 uppercase text-center font-serif text-xl font-bold">
-                Register now
-            </div>
-        </div>
-
-        -->
-
-        <!-- Articles - Lives -->
-        <div class="grow my-4 h-96 flex flex-row gap-4">
-            <div class="max-w-96 p-2 border border-gray-400 flex flex-col">
-                <div class="mt-2 h-60 border grow">
-                    <img class="object-cover w-full h-full" src="{{ asset('/assets/news-articles/first-person-in-space.jpg') }}" />
-                </div>
-                <div class="py-2 font-serif text-xl font-bold">
-                    First man going to space - Turns into woman halfway
-                </div>
-            </div>
-            <div class="max-w-96 p-2 border border-gray-400 flex flex-col">
-                <div class="mt-2 h-60 border grow">
-                    <img class="object-cover w-full h-full" src="{{ asset('/assets/news-articles/heroes-save-the-world.jpg') }}" />
-                </div>
-                <div class="py-2 font-serif text-xl font-bold">
-                    Nunc ut neque nec lorem varius cursus eget id magna.
-                </div>
-            </div>
-            <div class="max-w-96 p-2 border border-gray-400 flex flex-col">
-                <div class="mt-2 h-60 border grow">
-                    <img class="object-cover w-full h-full" src="#" />
-                </div>
-                <div class="py-2 font-serif text-xl font-bold">
-                    Etiam laoreet mauris dignissim erat hendrerit molestie.
-                </div>
-            </div>
-        </div>
-        
-        <!-- Articles - Music -->
-        <div class="grow    my-4 h-96 flex flex-row gap-4">
-            <div class="max-w-96 p-2 border border-gray-400 flex flex-col">
-                <div class="mt-2 h-60 border grow">
-                    <img class="object-cover w-full h-full" src="{{ asset('/assets/news-articles/first-person-in-space.jpg') }}" />
-                </div>
-                <div class="py-2 font-serif text-xl font-bold">
-                    First man going to space - Turns into woman halfway
-                </div>
-            </div>
-            <div class="max-w-96 p-2 border border-gray-400 flex flex-col">
-                <div class="mt-2 h-60 border grow">
-                    <img class="object-cover w-full h-full" src="{{ asset('/assets/news-articles/heroes-save-the-world.jpg') }}" />
-                </div>
-                <div class="py-2 font-serif text-xl font-bold">
-                    Nunc ut neque nec lorem varius cursus eget id magna.
-                </div>
-            </div>
-            <div class="max-w-96 p-2 border border-gray-400 flex flex-col">
-                <div class="mt-2 h-60 border grow">
-                    <img class="object-cover w-full h-full" src="#" />
-                </div>
-                <div class="py-2 font-serif text-xl font-bold">
-                    Etiam laoreet mauris dignissim erat hendrerit molestie.
-                </div>
-            </div>
         </div>
 
     </div>
