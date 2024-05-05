01a7a0c28183ca793632521df681019a1db6b07c
oholcurse <oholcurse@shell.domeneshop.no>
2024-05-05 14:50:22 +0200

    Added news article comments and replies section

--- app/Livewire/News/Comments.php
+++ app/Livewire/News/Comments.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\News;
 
 use Livewire\Component;
+use Auth;
 
 use App\Models\NewsArticleComment;
 
@@ -12,7 +13,7 @@ class Comments extends Component
     public $comments;
 
     public $user_comment;
-    public $replying_to;
+    public $replies_to = null;
 
 
     public function mount($article)
@@ -31,7 +32,42 @@ class Comments extends Component
 
     public function postComment()
     {
+        $validated = $this->validate([
+            'user_comment' => 'required|string|min:1|max:250',
+            'replies_to' => 'nullable|numeric'
+        ]);
 
+        NewsArticleComment::create(
+            [
+                'article_id' => $this->article,
+                'user_id' => Auth::user()->id,
+                'replies_to' => $this->replies_to,
+                'comment' => $this->user_comment,
+            ]
+        );
+
+        $this->clearForm();
+        $this->getComments();
+    }
+
+    public function setReply($id)
+    {
+        $this->replying_to = $id;
+    }
+
+    public function deleteComment($id)
+    {
+        $comment = NewsArticleComment::find($id);
+        $comment->comment = 'Comment deleted by user';
+        $comment->save();
+
+        $this->getComments();
+    }
+
+    public function clearForm()
+    {
+        $this->user_comment = null;
+        $this->replying_to = null;
     }
 
     public function render()
